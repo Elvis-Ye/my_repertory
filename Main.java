62ba092d46e193477c63bb1dca607afd807d0a48
elvis <rojohe@163.com>
2018-01-30 13:25:06 +0800

    New Java File

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	    System.out.println("hello world");
+	    //System.out.println("hello Baidu");
+	    //System.out.println("hello Google");
+
+	}
+
+}
