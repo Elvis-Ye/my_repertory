5bf8902a81c5b48565ae22c5cbf5836a74af4459
elvis <rojohe@163.com>
2018-01-30 13:26:48 +0800

    submit code

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-	    System.out.println("hello world");
-	    //System.out.println("hello Baidu");
-	    //System.out.println("hello Google");
+	    System.out.println("hello");
+	    System.out.println("hello Baidu");
+	    System.out.println("hello Google");
 
 	}
 
