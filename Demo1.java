b178e75509581d356d69652afd56653a2141eeb2
elvis <rojohe@163.com>
2018-02-05 03:49:02 +0800

    chanage two file

--- Demo1.java
+++ Demo1.java
@@ -5,5 +5,6 @@ import java.util.Date;
 public class Demo1 {
 	public static void main(String[] args) {
        System.out.println("new date"+new Date());
+       System.out.println("remote repository");
 	}
 }
